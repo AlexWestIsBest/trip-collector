5fe718fc3f5244274f9bc916310f6ddd74d2592f
Alex Westerlund <alexwesterlund@Alexs-MacBook-Pro.local>
2023-02-06 00:04:02 -0500

    Adds detail page

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('trips/', views.trips_index, name='index')
+    path('trips/', views.trips_index, name='index'),
+    path('trips/<int:trip_id>/', views.trips_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,24 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-class Trip:
-    def __init__(self, place, age, story):
-        self.place = place
-        self.age = age
-        self.story = story
-
-trips = [
-    Trip('Spain', 21, 'Visiting Barcelona'),
-    Trip('Estonia', 22, 'Exploring the Baltics with friends'),
-    Trip('Croatia', 23, 'Plus a small trip up to Croatia'),
-]
+from .models import Trip
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def trips_index(request):
+    trips = Trip.objects.all()
     return render(request, 'trips/index.html', {'trips': trips})
+
+def trips_detail(request, trip_id):
+    trip = Trip.objects.get(id=trip_id)
+    return render(request, 'trips/detail.html', {'trip': trip})
